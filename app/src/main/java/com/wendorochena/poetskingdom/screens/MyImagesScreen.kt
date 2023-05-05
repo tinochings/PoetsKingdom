41dcc78a5a2ae2b179364ab44eabea69aa8cf13d
Tino <tinotendachings@gmail.com>
2023-05-05 18:13:46 +0200

    MyImagesScreen.kt is fully functional and ready for testing

--- app/src/main/java/com/wendorochena/poetskingdom/screens/MyImagesScreen.kt
+++ app/src/main/java/com/wendorochena/poetskingdom/screens/MyImagesScreen.kt
@@ -1,9 +1,14 @@
 package com.wendorochena.poetskingdom.screens
 
-import android.content.Context
 import android.content.Intent
+import androidx.activity.compose.BackHandler
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,9 +18,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,6 +33,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -32,55 +43,142 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.wendorochena.poetskingdom.ImageViewer
 import com.wendorochena.poetskingdom.R
+import com.wendorochena.poetskingdom.ui.theme.DefaultBackgroundColor
+import com.wendorochena.poetskingdom.ui.theme.DefaultColor
 import com.wendorochena.poetskingdom.ui.theme.PoetsKingdomTheme
+import com.wendorochena.poetskingdom.utils.UriUtils
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyImagesScreenApp(
-    modifier: Modifier = Modifier,
     myImagesViewModel: MyImagesViewModel = viewModel()
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-    ) {
-        TopRow(myImagesViewModel = myImagesViewModel)
-        if (myImagesViewModel.currentSelection == CurrentSelection.IMAGES)
-            ImagesView()
-        else
-            PoemImagesView(myImagesViewModel)
+    Scaffold(floatingActionButton = {
+                                    FloatingActionButton(myImagesViewModel = myImagesViewModel)
+    }, floatingActionButtonPosition = FabPosition.End) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .background(DefaultBackgroundColor)
+        ) {
+            if (myImagesViewModel.onImageLongPressed) {
+                BackHandler(true) {
+                    myImagesViewModel.setOnLongClick(false)
+                    myImagesViewModel.setFloatingButtonState(FloatingButtonState.ADDIMAGE)
+                }
+            }
+            TopRow(myImagesViewModel = myImagesViewModel)
+            if (myImagesViewModel.currentSelection == CurrentSelection.IMAGES)
+                ImagesView(myImagesViewModel = myImagesViewModel)
+            else
+                PoemImagesView(myImagesViewModel)
+
+        }
     }
+}
+
+@Composable
+fun FloatingActionButton(myImagesViewModel: MyImagesViewModel) {
+    val context = LocalContext.current
+
+    val imagePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetMultipleContents(),
+        onResult = { uriList ->
+            var counter = 0
+            while (counter < uriList.size) {
+                val uriUtils =
+                    UriUtils(context, uri = uriList[counter])
+                myImagesViewModel.copyToLocalFolder(
+                    uriUtils.getRealPathFromURI(),
+                    context = context
+                )
+                counter++
+            }
+        }
+    )
+    androidx.compose.material.FloatingActionButton(
+        onClick = { },
+        backgroundColor = DefaultColor
+    ) {
+        var onClick by remember {
+            mutableStateOf(false)
+        }
+        if (myImagesViewModel.floatingButtonStateVar == FloatingButtonState.ADDIMAGE) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_add_a_photo_24),
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(color = Color.White),
+                modifier = Modifier.clickable { onClick = true }
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_delete_24),
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(color = Color.White),
+                modifier = Modifier.clickable {
+                    if (myImagesViewModel.currentSelection == CurrentSelection.IMAGES) myImagesViewModel.deleteImages()
+                    else myImagesViewModel.deleteSavedPoems(context = context)
+                }
+            )
+        }
+        if (onClick) {
+            imagePicker.launch("image/*")
+            onClick = false
+        }
+    }
+
 }
 
 @Composable
 fun PoemImagesView(myImagesViewModel: MyImagesViewModel, modifier: Modifier = Modifier) {
     val onImageItemClick: @Composable (File) -> Unit = {
-        val imageIntent = Intent(LocalContext.current.applicationContext, ImageViewer::class.java)
-        val poemTitleTextView = it.name.split(".png")[0].replace('_', ' ')
-        imageIntent.putExtra("imageLoadType", "poem saved image")
-        imageIntent.putExtra("poemName", poemTitleTextView)
-        LocalContext.current.startActivity(imageIntent)
+        if (!myImagesViewModel.onImageLongPressed) {
+            val imageIntent =
+                Intent(LocalContext.current.applicationContext, ImageViewer::class.java)
+            val poemTitleTextView = it.name.split(".png")[0].replace('_', ' ')
+            imageIntent.putExtra("imageLoadType", "poem saved image")
+            imageIntent.putExtra("poemName", poemTitleTextView)
+            LocalContext.current.startActivity(imageIntent)
+        } else {
+            myImagesViewModel.savedPoemImages[it] = !myImagesViewModel.savedPoemImages[it]!!
+        }
     }
 
+    val onLongClick: (File) -> Unit = {
+        myImagesViewModel.savedPoemImages[it] = true
+        myImagesViewModel.setOnLongClick(true)
+        if (myImagesViewModel.floatingButtonStateVar != FloatingButtonState.DELETEIMAGE)
+            myImagesViewModel.setFloatingButtonState(FloatingButtonState.DELETEIMAGE)
+    }
     val imageFiles = myImagesViewModel.getThumbnails(LocalContext.current.applicationContext)
+    val imageFileKeys = imageFiles.keys.asSequence().sortedByDescending { it.lastModified() }
     LazyVerticalGrid(
         modifier = modifier.padding(top = 5.dp), columns = GridCells.Fixed(2),
         verticalArrangement = Arrangement.spacedBy(10.dp),
         horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        items(count = imageFiles.size) {
+        items(count = imageFileKeys.count()) {
             Column {
-                ImagesItem(imageFiles[it], modifier = Modifier.padding(3.dp), onImageItemClick)
-                TitleItem(imageFiles[it])
+                val file = imageFileKeys.elementAt(it)
+                ImagesItem(
+                    Pair(file, imageFiles[file]!!),
+                    modifier = Modifier.padding(3.dp),
+                    onImageItemClick,
+                    onLongClick,
+                    myImagesViewModel.onImageLongPressed
+                )
+                TitleItem(file)
             }
         }
     }
@@ -97,15 +195,15 @@ fun TitleItem(imageFile: File, modifier: Modifier = Modifier) {
             .removeRange(0, 1)
     Column {
         Box(modifier = modifier.height(50.dp), contentAlignment = Alignment.Center) {
-        Text(
-            text = titleName,
-            maxLines = 1,
-            modifier = Modifier
-                .fillMaxWidth(),
-            style = MaterialTheme.typography.h1,
-            textAlign = TextAlign.Center
-        )
-    }
+            Text(
+                text = titleName,
+                maxLines = 1,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                style = MaterialTheme.typography.h1,
+                textAlign = TextAlign.Center
+            )
+        }
         Text(
             text = dateText, maxLines = 1, modifier = Modifier
                 .fillMaxWidth()
@@ -118,48 +216,93 @@ fun TitleItem(imageFile: File, modifier: Modifier = Modifier) {
     }
 }
 
-@OptIn(ExperimentalGlideComposeApi::class)
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalFoundationApi::class)
 @Composable
-fun ImagesItem(imageFile: File, modifier: Modifier, onImageItemClick: @Composable (File) -> Unit) {
-    var imageClicked by remember {
-        mutableStateOf(false)
-    }
-    GlideImage(
-        model = imageFile.absolutePath, contentDescription = "",
-        modifier = modifier
-            .fillMaxWidth()
-            .height(150.dp)
-            .clickable { imageClicked = true },
-        contentScale = ContentScale.FillBounds
-    )
-    if (imageClicked) {
-        onImageItemClick.invoke(imageFile)
-        imageClicked = false
+fun ImagesItem(
+    imageFilePair: Pair<File, Boolean>,
+    modifier: Modifier,
+    onImageItemClick: @Composable (File) -> Unit,
+    onLongClick: (File) -> Unit,
+    isLongClicked: Boolean
+) {
+    var imageClicked by remember { mutableStateOf(false) }
+    Box(modifier = Modifier) {
+        if (isLongClicked) {
+            if (imageFilePair.second) {
+                Image(
+                    modifier = Modifier
+                        .width(25.dp)
+                        .height(25.dp)
+                        .zIndex(5f),
+                    painter = painterResource(id = R.drawable.check_mark),
+                    contentDescription = ""
+                )
+            } else {
+                Image(
+                    modifier = Modifier
+                        .width(25.dp)
+                        .height(25.dp)
+                        .zIndex(5f),
+                    painter = painterResource(id = R.drawable.circle_svg),
+                    contentDescription = ""
+                )
+            }
+        }
+        GlideImage(
+            model = imageFilePair.first.absolutePath, contentDescription = "",
+            modifier = modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .combinedClickable(
+                    enabled = true,
+                    onClick = { imageClicked = true },
+                    onLongClick = { onLongClick.invoke(imageFilePair.first) }),
+            contentScale = ContentScale.FillBounds
+        )
+        if (imageClicked) {
+            onImageItemClick.invoke(imageFilePair.first)
+            imageClicked = false
+        }
     }
 }
 
 @Composable
-fun ImagesView(modifier: Modifier = Modifier) {
+fun ImagesView(
+    modifier: Modifier = Modifier, myImagesViewModel: MyImagesViewModel
+) {
     val onImageItemClick: @Composable (File) -> Unit = {
-        val imageIntent = Intent(LocalContext.current.applicationContext, ImageViewer::class.java)
-        imageIntent.putExtra("imageLoadType", "image")
-        imageIntent.putExtra("imagePath", it.absolutePath)
-        LocalContext.current.startActivity(imageIntent)
+        if (!myImagesViewModel.onImageLongPressed) {
+            val imageIntent =
+                Intent(LocalContext.current.applicationContext, ImageViewer::class.java)
+            imageIntent.putExtra("imageLoadType", "image")
+            imageIntent.putExtra("imagePath", it.absolutePath)
+            LocalContext.current.startActivity(imageIntent)
+        } else {
+            myImagesViewModel.imageFiles[it] = !myImagesViewModel.imageFiles[it]!!
+        }
     }
-    val imagesFolder = LocalContext.current.applicationContext.getDir(
-        stringResource(id = R.string.my_images_folder_name),
-        Context.MODE_PRIVATE
-    )
-    val imageFiles = imagesFolder?.listFiles()
+    val onLongClick: (File) -> Unit = {
+        myImagesViewModel.imageFiles[it] = true
+        myImagesViewModel.setOnLongClick(true)
+        if (myImagesViewModel.floatingButtonStateVar != FloatingButtonState.DELETEIMAGE)
+            myImagesViewModel.setFloatingButtonState(FloatingButtonState.DELETEIMAGE)
+    }
+    val imageFiles = myImagesViewModel.getImageFiles(LocalContext.current.applicationContext)
+    val imageFileKeys = imageFiles.keys.asSequence()
     LazyVerticalGrid(
         modifier = modifier.padding(top = 5.dp), columns = GridCells.Fixed(3),
         verticalArrangement = Arrangement.spacedBy(5.dp),
         horizontalArrangement = Arrangement.spacedBy(5.dp)
     ) {
-        if (imageFiles != null) {
-            items(count = imageFiles.size) {
-                ImagesItem(imageFiles[it], modifier = Modifier.padding(3.dp), onImageItemClick)
-            }
+        items(count = imageFiles.size) {
+            val file = imageFileKeys.elementAt(it)
+            ImagesItem(
+                Pair(file, imageFiles[file]!!),
+                modifier = Modifier.padding(3.dp),
+                onImageItemClick,
+                onLongClick,
+                myImagesViewModel.onImageLongPressed
+            )
         }
     }
 }
@@ -179,7 +322,11 @@ fun TopRow(modifier: Modifier = Modifier, myImagesViewModel: MyImagesViewModel) 
     ) {
         Row(modifier = Modifier
             .weight(1f)
-            .clickable { onClick.invoke(CurrentSelection.IMAGES) }) {
+            .clickable {
+                if (!myImagesViewModel.onImageLongPressed) onClick.invoke(
+                    CurrentSelection.IMAGES
+                )
+            }) {
             if (myImagesViewModel.currentSelection == CurrentSelection.IMAGES) {
                 Image(
                     modifier = Modifier
@@ -200,7 +347,11 @@ fun TopRow(modifier: Modifier = Modifier, myImagesViewModel: MyImagesViewModel) 
         }
         Row(modifier = Modifier
             .weight(1f)
-            .clickable { onClick.invoke(CurrentSelection.POEMS) }) {
+            .clickable {
+                if (!myImagesViewModel.onImageLongPressed) onClick.invoke(
+                    CurrentSelection.POEMS
+                )
+            }) {
             if (myImagesViewModel.currentSelection == CurrentSelection.POEMS) {
                 Image(
                     modifier = Modifier
--- app/src/main/java/com/wendorochena/poetskingdom/screens/MyImagesViewModel.kt
+++ app/src/main/java/com/wendorochena/poetskingdom/screens/MyImagesViewModel.kt
@@ -3,56 +3,204 @@ package com.wendorochena.poetskingdom.screens
 import android.content.Context
 import android.util.Log
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.wendorochena.poetskingdom.R
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 enum class CurrentSelection {
     IMAGES, POEMS
+}
+
+enum class FloatingButtonState {
+    ADDIMAGE, DELETEIMAGE
 }
 
 class MyImagesViewModel : ViewModel() {
 
     var currentSelection by mutableStateOf(CurrentSelection.IMAGES)
         private set
-//    var poem
+    var floatingButtonStateVar by mutableStateOf(FloatingButtonState.ADDIMAGE)
+        private set
+    var onImageLongPressed by mutableStateOf(false)
+        private set
+    var savedPoemImages = mutableStateMapOf<File, Boolean>()
+        private set
+    var imageFiles = mutableStateMapOf<File, Boolean>()
+        private set
 
     fun setSelection(selection: CurrentSelection) {
         currentSelection = selection
     }
 
+    fun setFloatingButtonState(state: FloatingButtonState) {
+        floatingButtonStateVar = state
+    }
+
+    private fun addAllFiles(
+        arrayList: ArrayList<File>?,
+        listFiles: Array<File>?,
+        isImages: Boolean
+    ) {
+        if (isImages) {
+            for (file in listFiles!!) {
+                imageFiles[file] = false
+            }
+        } else {
+            for (file in arrayList!!) {
+                savedPoemImages[file] = false
+            }
+        }
+    }
+
+    fun getImageFiles(context: Context): MutableMap<File, Boolean> {
+        if (imageFiles.isEmpty()) {
+            val imagesFolder = context.getDir(
+                context.getString(R.string.my_images_folder_name),
+                Context.MODE_PRIVATE
+            )
+
+            if (imagesFolder?.listFiles() != null)
+                addAllFiles(null, imagesFolder.listFiles()!!, true)
+        }
+        return imageFiles
+    }
+
+    fun setOnLongClick(boolean: Boolean) {
+        onImageLongPressed = boolean
+    }
+
+    /**
+     * Deletes image iles
+     */
+    fun deleteImages() {
+        val filesToDelete = imageFiles.filter { it.value }
+        for (entry in filesToDelete) {
+            if (entry.key.delete())
+                imageFiles.remove(entry.key)
+            else
+                Log.e("Failed to remove file: ", entry.key.name)
+        }
+        onImageLongPressed = false
+        setFloatingButtonState(FloatingButtonState.ADDIMAGE)
+    }
+
+    /**
+     * Deletes saved poem images
+     *
+     * @param context application context
+     */
+    fun deleteSavedPoems(context: Context) {
+        val filesToDelete = savedPoemImages.filter { it.value }
+        for (entry in filesToDelete) {
+            try {
+                val file = entry.key
+                val splitString = file.name.split(File.separator)
+                val poemName = splitString[splitString.size - 1]
+                val savedImagesFilePath = context.getDir(
+                    context.getString(R.string.saved_images_folder_name),
+                    Context.MODE_PRIVATE
+                )
+                val fullPathToDelete = File(
+                    savedImagesFilePath.absolutePath + File.separator + poemName.replace(
+                        ".png",
+                        ""
+                    )
+                )
+                if (fullPathToDelete.exists())
+                    fullPathToDelete.deleteRecursively()
+                else {
+                    Log.e("Failed to remove file: ", entry.key.name)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e("Failed to remove file: ", entry.key.name)
+            }
+        }
+        onImageLongPressed = false
+        setFloatingButtonState(FloatingButtonState.ADDIMAGE)
+    }
+
     /**
      * Gets an arraylist containing thumbnails to display
      */
-    fun getThumbnails(context: Context): ArrayList<File> {
+    fun getThumbnails(context: Context): MutableMap<File, Boolean> {
         val arrayListToRet = ArrayList<File>()
-        val savedImagesFolder =
-            context.getDir(
-                context.getString(R.string.saved_images_folder_name),
-                Context.MODE_PRIVATE
-            )
-        val thumbnailsFolder =
-            context.getDir(context.getString(R.string.thumbnails_folder_name), Context.MODE_PRIVATE)
+
+        if (savedPoemImages.isEmpty()) {
+            val savedImagesFolder =
+                context.getDir(
+                    context.getString(R.string.saved_images_folder_name),
+                    Context.MODE_PRIVATE
+                )
+            val thumbnailsFolder =
+                context.getDir(
+                    context.getString(R.string.thumbnails_folder_name),
+                    Context.MODE_PRIVATE
+                )
+            try {
+                val savedImageFiles = savedImagesFolder.listFiles()?.toMutableList()
+                if (savedImageFiles != null) {
+                    for (file in savedImageFiles) {
+                        val thumbnailFile = File(
+                            thumbnailsFolder?.absolutePath + File.separator + file.name + ".png"
+                        )
+                        if (thumbnailFile.exists()) {
+                            arrayListToRet.add(File(thumbnailFile.absolutePath))
+                        } else {
+                            Log.e("No Such Thumbnail", file.name)
+                        }
+                    }
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+            addAllFiles(arrayListToRet, null, false)
+        }
+        return savedPoemImages
+    }
+    /**
+     * @param imagePath the path to the image that we want to copy
+     *
+     * This function copies any selected image or images to our own local folder
+     */
+    fun copyToLocalFolder(imagePath: String?, context: Context) {
+        val localImageFolder = context.getDir(context.getString(R.string.my_images_folder_name), Context.MODE_PRIVATE)
         try {
-            val savedImageFiles = savedImagesFolder.listFiles()?.toMutableList()
-            if (savedImageFiles != null) {
-                savedImageFiles.sortByDescending { it.lastModified() }
-                for (file in savedImageFiles) {
-                    val thumbnailFile = File(
-                        thumbnailsFolder?.absolutePath + File.separator + file.name + ".png"
-                    )
-                    if (thumbnailFile.exists()) {
-                        arrayListToRet.add(File(thumbnailFile.absolutePath))
-                    } else {
-                        Log.e("No Such Thumbnail", file.name)
+            var newFileName = ""
+            if (imagePath != null) {
+                val splitString = imagePath.split("/")
+                newFileName = splitString[splitString.size - 1]
+            }
+            if (newFileName != "") {
+                val imageToAdd = File(localImageFolder?.path + File.separator + newFileName)
+
+                if (!imageToAdd.exists()) {
+                    if (imageToAdd.createNewFile()) {
+                        val imageFile = imagePath?.let { File(it) }
+                        val inputStream = FileInputStream(imagePath)
+                        val outputStream = FileOutputStream(imageToAdd)
+
+                        if (imageFile != null) {
+                            outputStream.channel.transferFrom(
+                                inputStream.channel,
+                                0,
+                                imageFile.totalSpace
+                            )
+                        }
+                        outputStream.close()
+                        inputStream.close()
+                        imageFiles[imageToAdd] = false
+                        println("sakjfhksajdhskjd")
                     }
                 }
             }
-        } catch (exception: Exception) {
-            exception.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        return arrayListToRet
     }
 }
