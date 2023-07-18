3f5952630832d8ede76ad6fd6b54d5d4dbc45914
Tino <tinotendachings@gmail.com>
2023-07-18 19:14:09 +0200

    Added new fonts

--- app/src/main/java/com/wendorochena/poetskingdom/utils/TypefaceHelper.kt
+++ app/src/main/java/com/wendorochena/poetskingdom/utils/TypefaceHelper.kt
@@ -272,35 +272,52 @@ class TypefaceHelper {
                         R.font.manheart_earthman_font
                     )
                 }
+
                 "woodland_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.woodland_font)
                 }
+
                 "dearest_winnie_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.dearest_winnie_font)
                 }
+
                 "x_typewriter_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.x_typewriter)
                 }
+
                 "turpis_serif_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.turpis_serif)
                 }
+
                 "santello_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.santello)
                 }
+
                 "handwritten_madness_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.handwritten_madness)
                 }
+
                 "desard_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.desard)
                 }
+
                 "helvetica_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.helvetica)
                 }
+
                 "arial_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.arial)
                 }
+
                 "calibri_font" -> {
                     return ResourcesCompat.getFont(applicationContext, R.font.calibri)
+                }
+
+                "fathia_font" -> {
+                    return ResourcesCompat.getFont(applicationContext, R.font.fathia)
+                }
+                "cursivesans_book_font" -> {
+                    return ResourcesCompat.getFont(applicationContext, R.font.cursivesans_book)
                 }
             }
             return DEFAULT
@@ -527,35 +544,52 @@ class TypefaceHelper {
                 "manheart_earthman" -> {
                     return FontFamily(Font(R.font.manheart_earthman))
                 }
+
                 "woodland" -> {
                     return FontFamily(Font(R.font.woodland))
                 }
+
                 "dearest_winnie" -> {
                     return FontFamily(Font(R.font.dearest_winnie))
                 }
+
                 "x_typewriter" -> {
                     return FontFamily(Font(R.font.x_typewriter))
                 }
+
                 "turpis_serif" -> {
                     return FontFamily(Font(R.font.turpis_serif))
                 }
+
                 "santello" -> {
                     return FontFamily(Font(R.font.santello))
                 }
+
                 "handwritten_madness" -> {
                     return FontFamily(Font(R.font.handwritten_madness))
                 }
+
                 "desard" -> {
                     return FontFamily(Font(R.font.desard))
                 }
+
                 "helvetica" -> {
                     return FontFamily(Font(R.font.helvetica))
                 }
+
                 "arial" -> {
                     return FontFamily(Font(R.font.arial))
                 }
+
                 "calibri" -> {
                     return FontFamily(Font(R.font.calibri))
+                }
+
+                "fathia" -> {
+                    return FontFamily(Font(R.font.fathia))
+                }
+                "cursivesans_book" -> {
+                    return FontFamily(Font(R.font.cursivesans_book))
                 }
             }
             return FontFamily(DEFAULT)
