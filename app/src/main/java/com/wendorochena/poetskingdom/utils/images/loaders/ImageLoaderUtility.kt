b2e2ac7daf0844d7477d1a9db247e42dd02ee9c3
Tino <tinotendachings@gmail.com>
2025-06-11 19:31:32 +0200

    feature: Tested and functional image loader

--- app/src/main/java/com/wendorochena/poetskingdom/utils/images/loaders/ImageLoaderUtility.kt
+++ app/src/main/java/com/wendorochena/poetskingdom/utils/images/loaders/ImageLoaderUtility.kt
@@ -1,16 +1,22 @@
 package com.wendorochena.poetskingdom.utils.images.loaders
 
 import android.content.Context
+import android.util.Log
 import coil3.imageLoader
 import coil3.memory.MemoryCache
 import coil3.request.CachePolicy
 import coil3.request.ImageRequest
-import com.wendorochena.poetskingdom.exceptions.images.ImageFileNotFoundException
+import coil3.request.error
+import coil3.request.placeholder
+import com.wendorochena.poetskingdom.R
+import com.wendorochena.poetskingdom.utils.files.cache.FileCacheUtil
+import com.wendorochena.poetskingdom.utils.files.cache.FileCacheUtility
 import com.wendorochena.poetskingdom.utils.generators.ImageCacheKeyGen
 import com.wendorochena.poetskingdom.utils.generators.contracts.ImageFolderType
 import com.wendorochena.poetskingdom.utils.images.loaders.contracts.ImageLoaderResourceManager
-import com.wendorochena.poetskingdom.utils.images.models.ImageItem
+import com.wendorochena.poetskingdom.utils.parallelism.images.executors.contracts.ImageRequestsCacheExecutor
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
 
@@ -21,7 +27,12 @@ class ImageLoaderUtility(val ioDispatcher: CoroutineDispatcher,
                          override val imageFolderType: ImageFolderType
 ) : ImageLoaderResourceManager {
 
-    private val imageCacheKeyGen : ImageCacheKeyGen = ImageCacheKeyGen()
+    private val imageCacheKeyGen: ImageCacheKeyGen = ImageCacheKeyGen(imageFolderType = imageFolderType)
+    private val cacheMissTag = "IMAGE_CACHE_MISS"
+    private val missingIndexTag = "IMAGE_INDEX_INVALID"
+    private val imageThumbnailsCacheName = "compressed_image_thumbnails"
+    private val myPoemsThumbnailsCacheName = "compressed_my_image_thumbnails"
+    private lateinit var sortedCacheFiles : List<File>
 
     /**
      * Clear memory and disk caches
@@ -31,45 +42,143 @@ class ImageLoaderUtility(val ioDispatcher: CoroutineDispatcher,
         context.imageLoader.diskCache?.clear()
         context.imageLoader.memoryCache?.clear()
         imageCacheKeyGen.invalidateCache(context)
+        val fileCacheUtility : FileCacheUtil = FileCacheUtility()
+        when(imageFolderType) {
+            ImageFolderType.IMAGES -> {
+                fileCacheUtility.invalidateCacheFolder(context = context, cacheName = imageThumbnailsCacheName)
+            }
+            ImageFolderType.POEM_THUMBNAILS -> {
+                fileCacheUtility.invalidateCacheFolder(context = context, cacheName =  myPoemsThumbnailsCacheName)
+            }
+        }
     }
 
     override fun removeFromCache(cacheKey: String, context: Context) {
         context.imageLoader.memoryCache?.remove(MemoryCache.Key(key = cacheKey))
         context.imageLoader.diskCache?.remove(cacheKey)
+        val fileCacheUtility : FileCacheUtil = FileCacheUtility()
+        when(imageFolderType) {
+            ImageFolderType.IMAGES -> {
+                fileCacheUtility.deleteEntriesFromCacheFolder(context = context, cacheName = imageThumbnailsCacheName, filesToDelete = arrayOf(cacheKey))
+            }
+            ImageFolderType.POEM_THUMBNAILS -> {
+                fileCacheUtility.deleteEntriesFromCacheFolder(context = context, cacheName = myPoemsThumbnailsCacheName, filesToDelete = arrayOf(cacheKey))
+            }
+        }
+    }
+
+    /**
+     * Insert image into cache
+     */
+    override suspend fun insertIntoCache(
+        context: Context,
+        filesToCache: Array<File>,
+        imageRequestsCacheExecutor: ImageRequestsCacheExecutor
+    ): List<ImageRequest> {
+        return imageRequestsCacheExecutor.executePreloadedFiles(filesToCache = filesToCache, imageFolderType = imageFolderType)
     }
 
     override suspend fun loadAllImages(
         context: Context,
         ioDispatcher: CoroutineDispatcher,
-        imagesDirectory : File
-    ) : List<ImageRequest>? {
-
+        imagesDirectoryFolderSize: Int,
+    ): List<ImageRequest> {
+        val retrievedCachedImages = ArrayList<ImageRequest>()
         return withContext(ioDispatcher) {
             try {
-                return@withContext imagesDirectory.listFiles()?.sortedByDescending { it.lastModified() }
-                    ?.mapNotNull { file: File? ->
-                        if (file != null)
-                            return@mapNotNull ImageItem(imageCacheKeyGen.generateKey(context), file)
-                        else
-                            null
-                    }?.map { imageItem: ImageItem ->
-                        val imageRequest: ImageRequest =
-                            ImageRequest.Builder(context).data(imageItem.imageFile.absolutePath)
-                                .coroutineContext(ioDispatcher)
-                                .fetcherCoroutineContext(ioDispatcher)
-                                .decoderCoroutineContext(ioDispatcher)
-                                .memoryCacheKey(imageItem.id.toString())
-                                .diskCacheKey(imageItem.id.toString())
-                                .diskCachePolicy(CachePolicy.ENABLED)
-                                .memoryCachePolicy(CachePolicy.ENABLED).build()
-                        return@map imageRequest
+                val lastKnownKey =  imageCacheKeyGen.retrieveLastKnownCachedKey(context)
+                val endIndex = lastKnownKey - (imagesDirectoryFolderSize - 1)
+                if (endIndex >= 0 && imagesDirectoryFolderSize > 0) {
+                    for (i in lastKnownKey  downTo endIndex) {
+                        val cachedValue =
+                            context.imageLoader.memoryCache?.get(MemoryCache.Key("$i"))
+
+                        if (cachedValue != null) {
+                            //cache hit
+                            val imageRequest = buildImageRequest(context = context, data = cachedValue.image, key = "$i", ioDispatcher = ioDispatcher)
+                            retrievedCachedImages.add(imageRequest)
+                        } else {
+                            //cache miss
+                            val ithIndex = i - lastKnownKey + imagesDirectoryFolderSize - 1
+                            Log.i(
+                                cacheMissTag,
+                                "Failed to retrieve index $ithIndex for folder: $imageFolderType, retrieving from disk"
+                            )
+                            buildImageRequestFromDisk(
+                                context = context,
+                                imageFileNumber = ithIndex,
+                                key = "$i",
+                            )?.let {
+                                retrievedCachedImages.add(
+                                    it
+                                )
+                            }
+                        }
                     }
+                }
             } catch (_: SecurityException) {
-
-            } catch (_: ImageFileNotFoundException) {
-
             }
-            return@withContext null
+            return@withContext retrievedCachedImages
+        }
+    }
+
+    /**
+     * Lazily retrieves the correct cache directory files and sorts them in ascending order of last modified.
+     *
+     */
+    private fun retrieveCacheDirectoryFiles(context: Context) : List<File>{
+        if (!::sortedCacheFiles.isInitialized){
+            sortedCacheFiles = when(imageFolderType) {
+                ImageFolderType.IMAGES -> FileCacheUtility().retrieveCacheDirectory(context = context, imageThumbnailsCacheName)!!.listFiles()!!.sortedBy { it.lastModified() }
+                ImageFolderType.POEM_THUMBNAILS -> FileCacheUtility().retrieveCacheDirectory(context = context, myPoemsThumbnailsCacheName)!!.listFiles()!!.sortedBy { it.lastModified() }
+            }
+        }
+
+        return sortedCacheFiles
+    }
+    /**
+     *
+     * @param context
+     * @param imageFileNumber
+     * @param key
+     */
+    private fun buildImageRequestFromDisk(
+        context: Context,
+        imageFileNumber: Int,
+        key: String,
+    ): ImageRequest? {
+        val cacheDirectoryFiles = retrieveCacheDirectoryFiles(context)
+
+        if (cacheDirectoryFiles.size > imageFileNumber) {
+            return buildImageRequest(context = context, data = cacheDirectoryFiles[imageFileNumber].absolutePath, key = key, ioDispatcher = ioDispatcher)
+        }
+        Log.w(
+            missingIndexTag, "$imageFileNumber index exceeds the maximum amount of " +
+                    "cached images for folder: $imageFolderType. There is a total of ${cacheDirectoryFiles.size} files"
+        )
+        return null
+    }
+
+
+    companion object  {
+
+        fun buildImageRequest(
+            context: Context,
+            data: Any,
+            key: String,
+            ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+        ): ImageRequest {
+            return ImageRequest.Builder(context).data(data)
+                .coroutineContext(ioDispatcher)
+                .fetcherCoroutineContext(ioDispatcher)
+                .decoderCoroutineContext(ioDispatcher)
+                .memoryCacheKey(key)
+                .diskCacheKey(key)
+                .diskCachePolicy(CachePolicy.ENABLED)
+                .error(R.drawable.baseline_broken_image_24)
+                .placeholder(R.drawable.baseline_placeholder)
+                .memoryCachePolicy(CachePolicy.ENABLED)
+                .build()
         }
     }
 }
---
+++ app/src/test/java/com/wendorochena/poetskingdom/utils/images/ImageLoaderUnitTests.kt
@@ -0,0 +1,219 @@
+package com.wendorochena.poetskingdom.utils.images
+
+import android.content.Context
+import coil3.Image
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.annotation.DelicateCoilApi
+import coil3.imageLoader
+import coil3.memory.MemoryCache
+import com.wendorochena.poetskingdom.utils.generators.ImageCacheKeyGen
+import com.wendorochena.poetskingdom.utils.generators.contracts.ImageFolderType
+import com.wendorochena.poetskingdom.utils.images.loaders.ImageLoaderUtility
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.BeforeClass
+import org.junit.Test
+import java.io.File
+
+/**
+ * For simplicity sake, both cache folders have the exact same amount of images.
+ *
+ *
+ */
+class ImageLoaderUnitTests {
+    private val imageThumbnailsCacheName = "compressed_image_thumbnails"
+    private val myPoemsThumbnailsCacheName = "compressed_my_image_thumbnails"
+
+    private val imagesFolder = File(
+            "../app/src/test/java/com/wendorochena/poetskingdom/cacheDirectoryImageLoader/$imageThumbnailsCacheName"
+            )
+
+    /**
+     * cached images are sorted in ascending order of last modified
+     */
+    private val sortedImagesFolderFiles = imagesFolder.listFiles()!!.sortedBy { it.lastModified() }
+
+    @Test
+    fun testInvalidDirectorySize() = runTest{
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns 0
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(mockedContext, dispatcher, -100)
+
+        assert(imageRequests.isEmpty())
+        testScheduler.advanceUntilIdle()
+    }
+
+    @Test
+    fun testValidDirectorySizeInvalidImageFolder()= runTest{
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns sortedImagesFolderFiles.size - 1
+        val invalidContext : Context = mockk<Context>()
+        setupMemoryCache(imageThumbnailsCacheName, 0 until imagesFolder.listFiles()!!.size, true, sortedImagesFolderFiles.size - 1)
+        every { invalidContext.cacheDir } returns File("invalid path")
+        assert(!invalidContext.cacheDir.exists())
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(invalidContext, dispatcher, 5)
+
+        assert(imageRequests.isEmpty())
+    }
+    /**
+     * Tests the path where every image is found in the cache
+     * N.B POSITIVE PATH TESTING
+     */
+    @Test
+    fun testLoadImagesCacheHit() = runTest {
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns sortedImagesFolderFiles.size - 1
+        setupMemoryCache(imageThumbnailsCacheName, 0 until imagesFolder.listFiles()!!.size, false, sortedImagesFolderFiles.size - 1)
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(mockedContext, dispatcher, imagesFolder.listFiles()!!.size)
+        testScheduler.advanceUntilIdle()
+        assert(imageRequests.size == sortedImagesFolderFiles.size)
+        for ((imageRequestsCounter, i) in (sortedImagesFolderFiles.size - 1 downTo 0).withIndex()){
+            assert(imageRequests[imageRequestsCounter].data is Image)
+            val dataAsImage = imageRequests[imageRequestsCounter].data as Image
+            assert(dataAsImage.toString() == sortedImagesFolderFiles[i].absolutePath)
+        }
+    }
+
+    /**
+     * Tests the path where every image is found in the cache when an arbitrarily large
+     * cache key i stored. A large cache key should not change the functionality of the cache
+     * retrieval
+     * N.B POSITIVE PATH TESTING
+     */
+    @Test
+    fun testLoadImagesCacheHitSmallRangeLargeLastKnownKey() = runTest {
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns 10000
+        setupMemoryCache(imageThumbnailsCacheName, 10000 - sortedImagesFolderFiles.size + 1 .. 10000, false, 10000)
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(mockedContext, dispatcher, imagesFolder.listFiles()!!.size)
+
+        assert(imageRequests.size == sortedImagesFolderFiles.size)
+        testScheduler.advanceUntilIdle()
+        for ((imageRequestsCounter, i) in (sortedImagesFolderFiles.size - 1 downTo 0).withIndex()){
+            assert(imageRequests[imageRequestsCounter].data is Image)
+            val dataAsImage = imageRequests[imageRequestsCounter].data as Image
+            assert(dataAsImage.toString() == sortedImagesFolderFiles[i].absolutePath)
+        }
+    }
+
+    /**
+     * Tests that loading from disk finds the correct image in the correct order.
+     */
+    @Test
+    fun testLoadImagesCacheMiss() = runTest {
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns sortedImagesFolderFiles.size - 1
+        setupMemoryCache(imageThumbnailsCacheName, 0 until imagesFolder.listFiles()!!.size, true, sortedImagesFolderFiles.size - 1)
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(mockedContext, dispatcher, imagesFolder.listFiles()!!.size)
+        testScheduler.advanceUntilIdle()
+
+        assert(imageRequests.size == sortedImagesFolderFiles.size)
+        for ((imageRequestsCounter, i) in (sortedImagesFolderFiles.size - 1 downTo 0).withIndex()){
+            assert(imageRequests[imageRequestsCounter].data is String)
+            val dataAsString = imageRequests[imageRequestsCounter].data
+
+            assert(dataAsString == sortedImagesFolderFiles[i].absolutePath)
+        }
+    }
+
+    /**
+     * Tests that loading from disk finds the correct image in the correct order when given a
+     * large range
+     */
+    @Test
+    fun testLoadImagesCacheMissLargeStartingRange() = runTest {
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns 20000
+        setupMemoryCache(imageThumbnailsCacheName, 20000 - imagesFolder.listFiles()!!.size + 1 .. 20000, true, 20000)
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(mockedContext, dispatcher, imagesFolder.listFiles()!!.size)
+        testScheduler.advanceUntilIdle()
+
+        assert(imageRequests.size == sortedImagesFolderFiles.size)
+        for ((imageRequestsCounter, i) in (sortedImagesFolderFiles.size - 1 downTo 0).withIndex()){
+            assert(imageRequests[imageRequestsCounter].data is String)
+            val dataAsString = imageRequests[imageRequestsCounter].data
+
+            assert(dataAsString == sortedImagesFolderFiles[i].absolutePath)
+        }
+    }
+
+    /**
+     * Tests that the ImageLoaderUtility can retrieve files from the cache and the disk in a
+     * single iteration
+     */
+    @Test
+    fun testLoadImagesHalfCacheHitHalfCacheMissLargeStartingRange() = runTest {
+        val dispatcher = StandardTestDispatcher(testScheduler)
+        every { anyConstructed<ImageCacheKeyGen>().retrieveLastKnownCachedKey(any()) } returns 20000
+        setupMemoryCache(imageThumbnailsCacheName, 19998 .. 19999, false, 20000)
+        setupMemoryCache(imageThumbnailsCacheName, 20000 .. 20000, true, 20000)
+        setupMemoryCache(imageThumbnailsCacheName, 19997 .. 19997, true, 20000)
+        val imageLoaderUtility = ImageLoaderUtility(dispatcher, ImageFolderType.IMAGES)
+        val imageRequests = imageLoaderUtility.loadAllImages(mockedContext, dispatcher, imagesFolder.listFiles()!!.size)
+        testScheduler.advanceUntilIdle()
+
+        assert(imageRequests.size == sortedImagesFolderFiles.size)
+        assert(imageRequests[0].data.toString() == sortedImagesFolderFiles[sortedImagesFolderFiles.size - 1].absolutePath)
+        assert(imageRequests[1].data.toString() == sortedImagesFolderFiles[sortedImagesFolderFiles.size - 2].absolutePath)
+        assert(imageRequests[2].data.toString() == sortedImagesFolderFiles[sortedImagesFolderFiles.size - 3].absolutePath)
+        assert(imageRequests[3].data.toString() == sortedImagesFolderFiles[0].absolutePath)
+    }
+    /**
+     * Create memory cache values in the correct  ascending order based on lastModified. The earliest
+     * created file is the first file and the latest edited file is the last
+     *
+     * In order to verify accuracy of an image we use the size of the file of each image as the
+     * uniquely identifying feature accessed by the width param
+     *
+     * At time of testing, no two files had the exact same file length
+     * @param range the range of cache keys from the start index until the end index
+     * @param nullify true if the range should be found in memory cache else false
+     * @param lastKnownKey the last known cache key
+     */
+    private fun setupMemoryCache(folder: String, range: IntRange, nullify : Boolean, lastKnownKey : Int) {
+        val memoryCacheKeys : HashSet<MemoryCache.Key> = HashSet()
+
+        range.forEach{ step ->
+            val currImageMock = mockk<Image>()
+            val index = step - lastKnownKey + sortedImagesFolderFiles.size - 1
+            every { currImageMock.toString() } returns sortedImagesFolderFiles[index].absolutePath
+            every { memoryCache[MemoryCache.Key("$step")] } returns if (nullify) null else MemoryCache.Value(currImageMock)
+            memoryCacheKeys.add(MemoryCache.Key("$step"))
+        }
+
+        every { memoryCache.keys } returns memoryCacheKeys
+    }
+
+    companion object {
+
+        var memoryCache: MemoryCache = mockk<MemoryCache>()
+        var mockedContext : Context = mockk<Context>()
+        @JvmStatic
+        @BeforeClass
+        fun setup() {
+            imageLoaderSetup()
+            mockkConstructor(ImageCacheKeyGen::class)
+            every { mockedContext.cacheDir } returns File("../app/src/test/java/com/wendorochena/poetskingdom/cacheDirectoryImageLoader")
+        }
+
+        @OptIn(DelicateCoilApi::class)
+        private fun imageLoaderSetup() {
+            mockkConstructor(ImageLoader::class)
+            val imageLoader = mockk<ImageLoader>()
+            every { imageLoader.memoryCache } returns memoryCache
+            every { mockedContext.imageLoader } returns imageLoader
+            every { anyConstructed<ImageLoader>().memoryCache } returns memoryCache
+            SingletonImageLoader.setUnsafe(imageLoader)
+        }
+    }
+}
