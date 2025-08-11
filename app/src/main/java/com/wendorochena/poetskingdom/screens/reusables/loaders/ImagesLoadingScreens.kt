24466d13d382bbe68088bad4c1241d143e5b1e98
Tino <tinotendachings@gmail.com>
2025-08-11 11:12:06 +0200

    Added an Image Shimmer loading screen

--- app/src/main/java/com/wendorochena/poetskingdom/screens/reusables/loaders/ImagesLoadingScreens.kt
+++ app/src/main/java/com/wendorochena/poetskingdom/screens/reusables/loaders/ImagesLoadingScreens.kt
@@ -1,5 +1,11 @@
 package com.wendorochena.poetskingdom.screens.reusables.loaders
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,10 +13,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material3.Card
@@ -19,7 +28,10 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +50,8 @@ fun ImagesNotification(
             .fillMaxSize()
             .background(color = Color.Black.copy(alpha = 0.3f))
             .padding(15.dp)
-            .clickable(enabled = false, interactionSource = null, indication = null, onClick = {}), contentAlignment = Alignment.BottomCenter
+            .clickable(enabled = false, interactionSource = null, indication = null, onClick = {}),
+        contentAlignment = Alignment.BottomCenter
     ) {
         Card(
             modifier = modifier.fillMaxWidth(), shape = RoundedRectangleOutline,
@@ -85,6 +98,54 @@ fun ImagesNotification(
             }
         }
     }
+}
+
+@Composable
+fun ImagesLoading(
+    modifier: Modifier = Modifier,
+    widthOfBrush: Int = 700,
+    durationMillis: Int = 1000,
+    angleOfAxisY: Float = 0f,
+    gridRowWidth : Int = 3
+) {
+    val shimmerColors = listOf(
+        Color.White.copy(alpha = 0.3f),
+        Color.White.copy(alpha = 0.5f),
+        Color.White.copy(alpha = 1.0f),
+        Color.White.copy(alpha = 0.5f),
+        Color.White.copy(alpha = 0.3f)
+    )
+
+    val transition = rememberInfiniteTransition()
+    val screenHeight = LocalWindowInfo.current.containerSize.height
+    val imageSize = LocalWindowInfo.current.containerSize.width / 3
+    val totalRows = screenHeight / imageSize
+    val totalImages = totalRows * gridRowWidth
+
+    val transitionAnimation = transition.animateFloat(
+        initialValue = 0f, targetValue = (durationMillis + widthOfBrush).toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = durationMillis,
+                easing = LinearEasing
+            ), repeatMode = RepeatMode.Restart
+        )
+    )
+    val brush = Brush.linearGradient(colors = shimmerColors, start = Offset(x = transitionAnimation.value - widthOfBrush, y = 0.0f),
+        end = Offset(x = transitionAnimation.value, y = angleOfAxisY))
+
+        LazyVerticalGrid(modifier = modifier.padding(top = 5.dp).fillMaxSize(), columns = GridCells.Fixed(gridRowWidth),
+            verticalArrangement = Arrangement.spacedBy(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)) {
+            items(totalImages){
+                Box(modifier = modifier) {
+                    Spacer(modifier = Modifier
+                        .padding(3.dp)
+                        .aspectRatio(1f)
+                        .background(brush = brush))
+                }
+            }
+        }
 }
 
 @Preview
